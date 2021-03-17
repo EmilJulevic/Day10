f93b893af4f6d2cb19ee9d60331016b470f2dd95
EmilJulevic <emiljulevic@live.com>
2021-03-17 22:06:46 +0100

    Adding ex01 folder

---
+++ ex01/src/index.js
@@ -0,0 +1,12 @@
+var myPet = {
+    species: "Dog",
+    name: "Rex",
+    legs: "4",
+    friends: "Mex, Jeck",
+}
+function myFunction(myObj) {
+    return myObj;
+}
+
+console.log(myFunction(myPet));
+module.exports = { myPet, myFunction };
