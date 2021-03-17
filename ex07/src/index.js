30de929ede1f84cb63d636e51e8dfdacf0295c81
EmilJulevic <emiljulevic@live.com>
2021-03-17 23:11:33 +0100

    Adding folders

---
+++ ex07/src/index.js
@@ -0,0 +1,12 @@
+var lion = {
+    name: "Simba",
+    legs: "4",
+    tails: "1",
+    roar: "roar-roar"
+};
+function myFunction() {
+    delete lion["roar"];
+    return lion;
+}
+console.log(myFunction("roar"));
+module.exports = myFunction;
